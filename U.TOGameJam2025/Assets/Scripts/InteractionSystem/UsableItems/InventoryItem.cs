1a01c045179289f7f87e9de47780c1cc0b329f7c
KianAquino <115228219+KianAquino@users.noreply.github.com>
2025-05-10 15:44:28 -0400

    InventoryItem cleanup

--- U.TOGameJam2025/Assets/Scripts/InteractionSystem/UsableItems/InventoryItem.cs
+++ U.TOGameJam2025/Assets/Scripts/InteractionSystem/UsableItems/InventoryItem.cs
@@ -5,12 +5,10 @@ using UnityEngine.Rendering;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 [RequireComponent(typeof(PlaySoundOnUse))]
-public class InventoryItem : MonoBehaviour, IUsable, IPointerClickHandler
+public class InventoryItem : MonoBehaviour, IUsable
 {
     [field: SerializeField] public UnityEvent OnUse {get; private set;}
 
-    private Camera _lootbagCamera;
-    private RawImage _lootbagTexture;
     private ItemState _itemState = ItemState.DROPPED;
 
     public void Use(GameObject actor)
@@ -26,31 +24,6 @@ public class InventoryItem : MonoBehaviour, IUsable, IPointerClickHandler
         Debug.Log("Using item: " + gameObject.name);
     }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Debug.Log("Clicked.");
-        if (_itemState == ItemState.IN_BAG)
-        {
-            Debug.Log("Clicked.");
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                _lootbagTexture.rectTransform,
-                eventData.position,
-                eventData.pressEventCamera,
-                out Vector2 localPos);
-
-            Rect rect = _lootbagTexture.rectTransform.rect;
-            float normalizedX = (localPos.x - rect.x) / rect.width;
-            float normalizedY = (localPos.y - rect.y) / rect.height;
-
-            Ray ray = _lootbagCamera.ViewportPointToRay(new Vector3(normalizedX, normalizedY, 0));
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                LootbagSystem.Instance.DropItem(this);
-            }
-        }
-    }
-
     public void Dropped()
     {
         Debug.Log(gameObject.name + " was dropped.");
@@ -61,13 +34,6 @@ public class InventoryItem : MonoBehaviour, IUsable, IPointerClickHandler
     {
         Debug.Log(gameObject.name + " was picked up.");
         _itemState = ItemState.IN_BAG;
-
-        if (_lootbagCamera == null)
-        {
-            Debug.Log("Loot Camera and Texture assigned.");
-            _lootbagTexture = UIHandler.Instance.LootbagTexture;
-            _lootbagCamera = LootbagSystem.Instance.LootbagCamera;
-        }
     }
 
     private enum ItemState
