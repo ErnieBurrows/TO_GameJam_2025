4412a1bcbdc9df3885fa6f60dd66ff9417f13379
KianAquino <115228219+KianAquino@users.noreply.github.com>
2025-05-11 14:49:06 -0400

    Sell Platform
    platform now tracks value of total items on it

---
+++ U.TOGameJam2025/Assets/Scripts/SellPlatform.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SellPlatform : MonoBehaviour
+{
+    // --------------------------------------------------
+    public static SellPlatform Instance;
+    // --------------------------------------------------
+    [SerializeField] TextMeshProUGUI _platformLabelTMPro;
+    // --------------------------------------------------
+    private List<LootItem> _items = new List<LootItem>();
+    private float _currentValue;
+    // --------------------------------------------------
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+    // --------------------------------------------------
+    private void Start()
+    {
+        RecalculateValue();
+    }
+    // --------------------------------------------------
+    private void OnTriggerEnter(Collider other)
+    {
+        LootItem lootItem = other.gameObject.GetComponent<LootItem>();
+        if (lootItem)
+        {
+            if (_items.Contains(lootItem)) return;          // List already has this item, do nothing
+
+            _items.Add(lootItem);
+            RecalculateValue();
+
+            Debug.Log("<SellPlatform> Item added.");
+        }
+    }
+    // --------------------------------------------------
+    private void OnTriggerExit(Collider other)
+    {
+        LootItem lootItem = other.gameObject.GetComponent<LootItem>();
+        if (lootItem)
+        {
+            if (!_items.Contains(lootItem)) return;          // List isn't in the list, do nothing
+
+            _items.Remove(lootItem);
+            RecalculateValue();
+
+            Debug.Log("<SellPlatform> Item removed.");
+        }
+    }
+    // --------------------------------------------------
+    private void RecalculateValue()
+    {
+        _currentValue = 0;
+
+        foreach (var item in _items)
+        {
+            _currentValue += item.Value;
+        }
+
+        _platformLabelTMPro.text = $"Value\n${_currentValue}";
+
+        Debug.Log("<SellPlatform> Current Value -> " + _currentValue);
+    }
+}
